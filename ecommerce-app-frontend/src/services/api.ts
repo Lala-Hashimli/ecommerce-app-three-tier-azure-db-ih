9d74441e07be69dbd55e4f659e2285bfd89b6c5e
Ubuntu <azureuser@ecommerce-vm-lalah-g3.3qu4bgnpw3xuzlifhps3i4rtbb.hx.internal.cloudapp.net>
2026-04-04 09:55:35 +0000

    Force frontend rebuild use /api

--- ecommerce-app-frontend/src/services/api.ts
+++ ecommerce-app-frontend/src/services/api.ts
@@ -13,6 +13,7 @@ import {
 } from '../types';
 
 const API_BASE_URL = process.env.REACT_APP_API_URL || '/api';
+console.log('NEW FRONTEND BUILD');
 
 // Create axios instance
 const api = axios.create({
